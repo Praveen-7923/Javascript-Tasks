bd6fd2d663feb95260ea5113e5c258c30f2919ac
Praveen-7923 <pravinprem56@gmail.com>
2026-09-12 22:00:44 +0530

    Add files via upload

---
+++ Js Day 7 Task/script.js
@@ -0,0 +1,10 @@
+
+function add($){
+document.getElementById("result").value+=$;
+}
+function cal(){
+document.getElementById("result").value=eval(document.getElementById("result").value);
+}
+function clr(){
+document.getElementById("result").value="";
+}
