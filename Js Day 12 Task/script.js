87b1e2b02c8f9a4f5a5d84306691b32ef94fbb1e
Praveen-7923 <pravinprem56@gmail.com>
2026-09-17 12:37:57 +0530

    Add files via upload

---
+++ Js Day 12 Task/script.js
@@ -0,0 +1,35 @@
+	
+
+	function add(){
+		num1=5;
+		num2=10;
+		add_ans=num1+num2;
+		document.getElementById("result").innerHTML = add_ans; 
+	// 	 <!-- document.write(add_ans);  -->
+	//
+     }
+	function sub(){
+		num1=15;
+		num2=10;
+		sub_ans=num1-num2;
+		document.getElementById("result").innerHTML = sub_ans;
+	}
+	function mul(){
+		num1=5;
+		num2=10;
+		mul_ans=num1*num2;
+		document.getElementById("result").innerHTML = mul_ans;
+	}
+	function div(){
+		num1=5;
+		num2=10;
+		div_ans=num1/num2;
+		document.getElementById("result").innerHTML = div_ans;
+	}
+	function mod(){
+	num1=10;
+	num2=30;
+	mod_ans=num1%num2;
+	document.getElementById("result").innerHTML = mod_ans;
+	}
+	
