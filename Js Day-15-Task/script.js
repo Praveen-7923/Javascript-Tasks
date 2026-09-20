d409648e1595d3fed0c30ca6a3ea199b98890a7e
Praveen-7923 <pravinprem56@gmail.com>
2026-09-20 21:42:32 +0530

    Add files via upload

---
+++ Js Day-15-Task/script.js
@@ -0,0 +1,4 @@
+
+        function showMessage() {
+            document.getElementById("message").innerHTML = "JavaScript is a versatile, high-level programming language that serves as one of the core technologies of the World Wide Web, enabling developers to implement complex features and dynamic, interactive behavior on web pages.";
+        }
